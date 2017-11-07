6d8cafa7e69863b0445b2fe74b2b9a4e4d6f4bca
Eric Marrone <emarrone17@gmail.com>
2017-11-06 23:09:29 -0500

    Complete part 1

--- part1.rb
+++ part1.rb
@@ -1,22 +1,26 @@
 class BankAccount
-  @@interest_rate = 0.0
+  @@interest_rate = 0.02
   @@accounts = []
 
   def initialize
-    @balance = 0
+    @balance = 0.0
   end
 
   #READERS
 
-    def balance
-      @balance
-    end
+  def balance
+    @balance
+  end
+
+  def self.accounts
+    @@accounts
+  end
 
 
   #WRITERS
-    def balance=(balance)
-      @balance = balance
-    end
+  def balance=(balance)
+    @balance = balance
+  end
 
 
   #instance methods
@@ -28,8 +32,8 @@ class BankAccount
     @balance -= number
   end
 
-  def self.create(account_name)
-    new_account = BankAccount.new(account_name)
+  def self.create
+    new_account = BankAccount.new
     @@accounts << new_account
     return new_account
   end
@@ -39,4 +43,37 @@ class BankAccount
     @@accounts.each do |account|
       total += account.balance
     end
+    return total
   end
+
+  def self.interest_time
+    @@accounts.each do |account|
+      account.balance *= 1.0 + @@interest_rate
+    end
+  end
+
+end
+
+my_account = BankAccount.create
+your_account = BankAccount.create
+
+# puts my_account.inspect
+# puts your_account.inspect
+
+puts BankAccount.accounts.inspect
+
+puts my_account.balance
+puts BankAccount.total_funds
+my_account.deposit(200)
+your_account.deposit(1000)
+puts my_account.balance
+puts your_account.balance
+puts BankAccount.total_funds
+BankAccount.interest_time
+puts my_account.balance
+puts your_account.balance
+puts BankAccount.total_funds
+my_account.withdraw(50)
+puts my_account.balance
+puts BankAccount.total_funds
+# puts my_account.inspect
